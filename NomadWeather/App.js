6eb6875fb766eebd3bf96079e87866b2abc461ad
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2024-09-23 23:23:01 +0900

    #2.4 Layout System

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -3,21 +3,10 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello! I made a RN App!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 1, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 28,
-  },
-});
