8d86e2ad70ec9b56a482a1c903cbb4ca2f45fd90
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2024-09-29 00:59:39 +0900

    #3.4 Paint To Dos

--- WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/App.js
@@ -7,6 +7,7 @@ import {
   View,
   TouchableOpacity,
   TextInput,
+  ScrollView,
 } from "react-native";
 
 export default function App() {
@@ -21,9 +22,7 @@ export default function App() {
     if (text === "") {
       return;
     }
-    const newTodos = Object.assign({}, toDos, {
-      [Date.now()]: { text, working },
-    });
+    const newTodos = { ...toDos, [Date.now()]: { text, work: working } };
     setToDos(newTodos);
     setText("");
   };
@@ -59,6 +58,13 @@ export default function App() {
         placeholder={working ? "Add a To Do" : "Where do you want to go?"}
         style={styles.input}
       />
+      <ScrollView>
+        {Object.keys(toDos).map((key) => (
+          <View style={styles.toDo} key={key}>
+            <Text style={styles.toDoText}>{toDos[key].text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -84,7 +90,15 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
   },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+  },
+  toDoText: { color: "white", fontSize: 16, fontWeight: "500" },
 });
--- WorkHardTravelHardApp/colors.js
+++ WorkHardTravelHardApp/colors.js
@@ -1,4 +1,5 @@
 export const theme = {
   bg: "black",
   grey: "#3a3D40",
+  toDoBg: "#5C5E60",
 };
